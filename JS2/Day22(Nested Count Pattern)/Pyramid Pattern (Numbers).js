3d78ea991171ad3e18075ed3345d28580c43fdba
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-04-26 17:29:55 +0530

    upadte

--- JS2/Day22(Nested Count Pattern)/Pyramid Pattern (Numbers).js
+++ JS2/Day22(Nested Count Pattern)/Pyramid Pattern (Numbers).js
@@ -1,17 +1,15 @@
 let num = 5;
-
-for (let i = 1; i <= num; i++) {
-  let row = " ".repeat(num - i);
-  
-  // Increasing sequence
-  for (let j = 1; j <= i; j++) {
-    row += j;
-  }
-  
-  // Decreasing sequence
-  for (let j = i - 1; j >= 1; j--) {
-    row += j;
-  }
-  
-  console.log(row);
+for(let i = 1; i <= num; i++){
+    let row = " ".repeat(num - i);
+///.  Incresing Order 
+    for(let j = 1; j <= i; j++){
+        row += j
+    }
+    
+    // Decreseing Order
+    for(let j = i - 1; j >= 1; j--){
+        row += j
+    }
+    console.log(row)
 }
+// row += String.fromCharCode(64 + j);  agar char print karane h to ye condition lga do 
