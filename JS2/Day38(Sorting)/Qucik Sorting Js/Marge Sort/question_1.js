b306cba090fbcdb880733d10fe514b1f28bfaf81
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-05-03 17:26:11 +0530

    update code

---
+++ JS2/Day38(Sorting)/Qucik Sorting Js/Marge Sort/question_1.js
@@ -0,0 +1,37 @@
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let results = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {    // thoda condition update kar di
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+function mergeSort(arr){
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+// Example usage:
+const unsortedArray = [5, 3, 8, 4, 2, 7, 1, 6];
+console.log(mergeSort(unsortedArray));  // Output: [1, 2, 3, 4, 5, 6, 7, 8]
