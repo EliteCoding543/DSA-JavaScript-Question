8d73dc0af9bd32bbbb42a5fe27542afdf2ba590e
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-05-09 22:17:06 +0530

    code

---
+++ JS2/Searching Algrothims/Binary Search/Find_Max_Elements.js
@@ -0,0 +1,22 @@
+let arr = [3, 4, 5, 6, 1, 2];
+
+function findMaxElements(arr){
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start < end){
+        mid = Math.floor((start + end)/ 2);
+
+        if(arr[mid] < arr[end]){
+            end = mid - 1
+        }
+
+        else {
+            start = mid;
+        }
+    }
+
+    return arr[start]
+}
+
+console.log(findMaxElements(arr));
