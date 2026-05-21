f6ebd7bd70225b4a589cf48d5daae1345db77ed6
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-05-21 13:02:43 +0530

    DOM New File Add

---
+++ JavaScript Problem/Number Solving/Count_primeNumber.js
@@ -0,0 +1,20 @@
+function countPrime(n){
+    let count = 0;
+
+    for(let num = 2; num <= n; num++){
+        let isPrime = true;
+
+        for(let i = 2; i <= Math.sqrt(num); i++){
+            if(num % i === 0){
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime){
+            count++;
+        }
+    }
+
+    return count;
+}
