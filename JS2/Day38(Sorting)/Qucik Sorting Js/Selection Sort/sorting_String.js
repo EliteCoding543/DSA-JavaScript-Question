d32880dd035f94bb41b881bb8ddf995a77f8d333
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-05-25 11:29:46 +0530

    Sorting String

---
+++ JS2/Day38(Sorting)/Qucik Sorting Js/Selection Sort/sorting_String.js
@@ -0,0 +1,19 @@
+function sortingString(str){
+    let arr = str.split("")
+
+    for(let i = 0; i < arr.length - 1; i++){
+        let minIndex = i;
+
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+
+    return arr.join("")
+}
+
+console.log(sortingString("FEDCBA"));
