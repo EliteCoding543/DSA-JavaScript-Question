1c2333c1dc3e64a7b1b59e91a281fd785c8c3084
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-06-02 14:47:30 +0530

    code

---
+++ Back_To_Js/Day62(Oops)/Carban_Board/app.js
@@ -0,0 +1,38 @@
+const columns = document.querySelectorAll(".task-list");
+
+function addTask() {
+  const input = document.getElementById("taskInput");
+
+  if (input.value === "") return;
+
+  const task = document.createElement("div");
+
+  task.classList.add("task");
+  task.innerText = input.value;
+
+  task.draggable = true;
+
+  task.addEventListener("dragstart", () => {
+    task.classList.add("dragging");
+  });
+
+  task.addEventListener("dragend", () => {
+    task.classList.remove("dragging");
+  });
+
+  document.getElementById("todo").appendChild(task);
+
+  input.value = "";
+}
+
+columns.forEach(column => {
+  column.addEventListener("dragover", e => {
+    e.preventDefault();
+
+    const dragging = document.querySelector(".dragging");
+
+    if (dragging) {
+      column.appendChild(dragging);
+    }
+  });
+});
