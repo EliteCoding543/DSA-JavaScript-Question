efe868a54f50ccc5471dbf6fba913af029c9136a
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-06-02 13:35:03 +0530

    Code

--- JavaScript Problem/SET & MAP (JS)/Map Using Problems/Map.js
+++ JavaScript Problem/SET & MAP (JS)/Map Using Problems/Map.js
@@ -1,17 +1,41 @@
-// / 2. has() in Map. Checks whether a key exists in a Map.
+let map = new Map();
 
-const user = new Map()
+map.set("name", "Shubham");
+map.set("age", 22);
 
-user.set("name", "Rahul")
-user.set("age", 15);
+console.log(map.has("name"));
 
-console.log(user.has("name"));
-console.log(user.has("15"));
+console.log(map.get("age"));
+console.log(map)
+map.set("program", "MERN Stack");
 
-// Value exist karti hai ya nahi kaise check kare
-// Method map.value
+console.log(map)
 
-console.log([...user.values()].includes("Rahul"));  // true;
+console.log(map.get("program"));
 
-// [...user.value()] Ye Map ki saari values ko array me convert kar deta hai.
+console.log(map.entries());
+console.log([...map.entries()]);
 
+console.log(map.keys());
+console.log([...map.keys()]);
+console.log(map.values());
+ console.log([...map.values()]);
+ 
+ for(let [key, value] of map){
+   console.log([key, value])
+ }
+
+
+//   Count of frq arr 
+
+let arr = [1, 1, 1, 3, 2, 2, 2, 2, 2,];
+let myMap = new Map()
+// Agar num nahi hai 0 maan lo aur 1 store karo
+for(let num of arr){
+  myMap.set(num, (myMap.get(num)  || 0) + 1)
+}
+
+//Agar num pehle se hai. uski frequency lao aur +1 karo
+for(let [key , value] of myMap){
+    console.log([key, value])
+}
