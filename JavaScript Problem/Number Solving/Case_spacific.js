575bd3977c25f28024d8e8359730610451754ecb
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-06-06 18:58:54 +0530

    code

--- JavaScript Problem/Number Solving/Case_spacific.js
+++ JavaScript Problem/Number Solving/Case_spacific.js
@@ -35,25 +35,4 @@ function sortCase(S) {
     return result.join('');
 }
 
-/* Do not change the code below */
-var readline = require("readline").createInterface(process.stdin);
-
-let inputArr = [];
-var lineNumber = -1;
-readline.on("line", readInputs);
-
-function readInputs(line) {
-  inputArr.push(line);
-  lineNumber++;
-  
-  // The system passes the string on the very first line (lineNumber == 0)
-  if (lineNumber == 0) {
-    logic();
-    readline.close();
-  }
-}
-
-function logic() {
-  // Read from inputArr[0] as seen in your editor's setup
-  console.log(sortCase(inputArr[0].trim()));
-}
+console.log(sortCase("abcd"));
