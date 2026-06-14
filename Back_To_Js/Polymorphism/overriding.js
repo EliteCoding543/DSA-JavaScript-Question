357934634622dfe14fd68fec13fe63c841e06686
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-06-14 16:54:34 +0530

    update

---
+++ Back_To_Js/Polymorphism/overriding.js
@@ -0,0 +1,50 @@
+class Parents{
+    constructor(name = "", age = ""){
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHi(){
+        console.log("Namste!");
+    }
+}
+
+// Child Class  extends <= this use protery inherti parents
+
+class Child extends Parents{
+    constructor(n, a, m){
+
+        super(n, a)
+        this.mobile = m;
+    }
+
+    sayHi(){
+        console.log("Hello!")
+    }
+
+}
+
+class GrandChlid extends Child{
+    constructor(n, a, m, c){
+        super(n, a, m)
+        this.cycle = c;
+    }
+
+    sayHi(){
+        console.log("Hor Londe");
+    }
+}
+
+const p1 = new Parents("Papa", 34)
+console.log(p1);
+
+const chlid1 = new Child("Sumit", 29, "Apple");
+console.log(chlid1);
+
+const GC = new GrandChlid("Abhi", 19, "Samsung", "Tricycle");
+console.log(GC)
+
+// Function Call 
+p1.sayHi();       // Overriding ye hi h jo alag alg call same fn 
+chlid1.sayHi();  // Overriding ye hi h jo alag alg call same fn 
+GC.sayHi();      // Overriding ye hi h jo alag alg call same fn 
