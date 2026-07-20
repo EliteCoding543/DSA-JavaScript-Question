6b23ab2ed81cf6f17530027e523cd808f7a5e521
EliteCoding543 <shubhamkashyap5432@gmail.com>
2026-07-20 18:47:03 +0530

    DOM

---
+++ DOM/Day1.js
@@ -0,0 +1,12 @@
+const collection = document.getElementsByClassName("box")
+const list = document.querySelectorAll(".box")
+const ele = document.getElementById("title");
+
+console.dir(ele);
+
+document.body.innerHTML +=
+
+'<div class="box"></div>';
+
+console.log(collection.length)
+console.log(list.length)
